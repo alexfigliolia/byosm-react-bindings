76e92596044cb7d9f4622e8bd2f0bc29a187b7be
Alex Figliolia <alexfigliolia@gmail.com>
2024-03-23 21:55:44 -0700

    Upload

--- src/createUseState.ts
+++ src/createUseState.ts
@@ -1,18 +1,16 @@
 import type { State } from "my-state";
-import { useLayoutEffect, useRef, useState } from "react";
+import { useLayoutEffect, useState } from "react";
 
 export const createUseState = <T extends State<any>>(state: T) => {
   return <S extends (state: T["state"]) => any>(selector: S) => {
     const [props, setProps] = useState<ReturnType<S>>(selector(state.state));
-    const ID = useRef("");
 
     useLayoutEffect(() => {
-      ID.current = state.subscribe((state) => {
+      const ID = state.subscribe((state) => {
         setProps(selector(state));
       });
-
       return () => {
-        state.unsubscribe(ID.current);
+        state.unsubscribe(ID);
       };
     }, []);
 
